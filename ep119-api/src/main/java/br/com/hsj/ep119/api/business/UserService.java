767484bfb975f21677486b2b525f4837b2642d61
haamilton <haamilton@gmail.com>
2016-05-04 16:06:17 -0300

    Alterações referentes ao xml de dados

--- ep119-api/src/main/java/br/com/hsj/ep119/api/business/UserService.java
+++ ep119-api/src/main/java/br/com/hsj/ep119/api/business/UserService.java
@@ -1,28 +1,51 @@
 package br.com.hsj.ep119.api.business;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.hsj.ep119.api.converter.Converter;
 import br.com.hsj.ep119.api.domain.User;
+import br.com.hsj.ep119.api.domain.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	private Converter converter;
+	
 	public List<User> find() {
 		
-		return createUsers();
-	}
-	
-	
-	private List<User> createUsers() {
-		List<User> users = new ArrayList<User>();
-		
-		for (int i = 1; i < 21; i++) {
-			users.add(new User(i, "Nome: " + i, "email" + i + "@gmail.com"));
+		try {
+			return createUsers();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		}
 		
-		return users;
+		return null;
 	}
+	
+	@SuppressWarnings("rawtypes")
+	private List<User> createUsers() throws IOException, URISyntaxException {
+//		List<User> users = new ArrayList<User>();
+//		User user = (User) converter.doUnMarshaling("/user.xml");
+//		users.add(user);
+		
+	
+		UserRepository repository = (UserRepository) converter.doUnMarshaling("/user.xml");
+		
+		return repository.getUsers();
+	}
+
+
+	public void setConverter(Converter converter) {
+		this.converter = converter;
+	}
+	
+	
 }
